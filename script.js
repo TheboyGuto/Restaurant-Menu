d7fb09f7f87c7d54880b8d91512c8a6511833f38
Gustavo <Gusrosa3@outlook.com.br>
2023-09-20 13:33:35 -0300

    Mudanças no html, estilos e array de items do cardapio incluida

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('teste')
+const items = [
+    {tipo : 'cafe', nome: 'panqueca com caramelo', preco: 15.99, descricao: 'Panqueca feita a mão coberta com caramelo', imagem: ''},
+    {tipo: 'almoco', nome: 'Alaminuta', preco: 32.00, descricao: 'Arroz,Feijão,Ovo,batata frita e bife', imagem: ''},
+    {tipo: 'Shake', nome: 'Shake de Baunilha', preco: 8, descricao: 'milkshake batido sabor baunilha', imagem: ''},
+    {tipo: 'Janta', nome: 'bife de Alcatra com saladas', preco: 40.00, descricao: 'Bife de alcatra frito em ervas finas com salada de espinafre', imagem: ''}
+]
+
+console.log(items)
