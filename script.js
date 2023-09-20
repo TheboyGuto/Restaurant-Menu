fac65ce3d3388f23cdf3d1228cb101714393f20e
Gustavo <Gusrosa3@outlook.com.br>
2023-09-20 17:02:06 -0300

    Filtro ao clicar no botão retornando

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const items = [
-    {tipo : 'cafe', nome: 'panqueca com caramelo', preco: 15.99, descricao: 'Panqueca feita a mão coberta com caramelo', imagem: ''},
-    {tipo: 'almoco', nome: 'Alaminuta', preco: 32.00, descricao: 'Arroz,Feijão,Ovo,batata frita e bife', imagem: ''},
-    {tipo: 'Shake', nome: 'Shake de Baunilha', preco: 8, descricao: 'milkshake batido sabor baunilha', imagem: ''},
-    {tipo: 'Janta', nome: 'bife de Alcatra com saladas', preco: 40.00, descricao: 'Bife de alcatra frito em ervas finas com salada de espinafre', imagem: ''}
+const items = [{
+    cafe : [{nome: 'panqueca com caramelo', preco: 15.99, descricao: 'Panqueca feita a mão coberta com caramelo', imagem: ''}],
+    almoco :  [{nome: 'Alaminuta', preco: 32.00, descricao: 'Arroz,Feijão,Ovo,batata frita e bife', imagem: ''}],
+    shake : [{nome: 'Shake de Baunilha', preco: 8, descricao: 'milkshake batido sabor baunilha', imagem: ''}],
+    janta : [{nome: 'bife de Alcatra com saladas', preco: 40.00, descricao: 'Bife de alcatra frito em ervas finas com salada de espinafre', imagem: ''}]}
 ]
 
 
+console.log(items);
 
-
-function filterItems(){
-
+function filterItems(item){
+    filter  = item.getAttribute("name")
+    
 }
+
